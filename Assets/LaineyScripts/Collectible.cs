41c8d9f741a805ee8072155a5a4baea4f854e49e
adelainerose <39711177+adelainerose@users.noreply.github.com>
2025-07-14 14:24:03 -0700

    Updated Collectible script; collects with key press "E"

--- Assets/LaineyScripts/Collectible.cs
+++ Assets/LaineyScripts/Collectible.cs
@@ -2,13 +2,27 @@ using UnityEngine;
 
 public class Collectible : MonoBehaviour
 {
-    [SerializeField] private string itemKey;  
+    [SerializeField] private string itemKey;
+    private bool playerInRange = false;
+
+    private void Update()
+    {
+        if (playerInRange)
+        {
+            if (Input.GetKeyDown("e"))
+            {
+                InventoryManager.Instance.AddItem(itemKey);
+                playerInRange = false;
+                Destroy(gameObject);
+            }
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (!other.CompareTag("Player")) return;
 
-        InventoryManager.Instance.AddItem(itemKey);
-        Destroy(gameObject);
+        playerInRange = true;
     }
+
 }
