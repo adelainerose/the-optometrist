6c0f51e3e3826e52d8fa3b0518a24b17871ca633
adelainerose <39711177+adelainerose@users.noreply.github.com>
2025-07-14 09:04:01 -0700

    added visual novel dialogue script

---
+++ Assets/LaineyScripts/VisualNovelDialogueManager.cs
@@ -0,0 +1,127 @@
+using UnityEngine;
+using TMPro;
+using Ink.Runtime;
+using System.Collections;
+using System.Collections.Generic;
+
+public class VisualNovelDialogueManager : MonoBehaviour
+{
+
+    [Header("Dialogue UI")]
+    [SerializeField] private TextAsset inkJSON;
+    [SerializeField] private GameObject dialoguePanel;
+    [SerializeField] private TextMeshProUGUI dialogueText;
+    [SerializeField] private TextMeshProUGUI displayNameText;
+    [SerializeField] private Animator portraitAnimator;
+    [SerializeField] private string LoadLevel;
+
+    [Header("Choices UI")]
+    [SerializeField] private GameObject[] choices;
+    private TextMeshProUGUI[] choicesText;
+
+    private Story currentStory;
+
+    public bool dialogueIsPlaying { get; private set; }
+
+    private const string SPEAKER_TAG = "speaker";
+    private const string PORTRAIT_TAG = "portrait";
+
+    private void Start()
+    {
+
+        choicesText = new TextMeshProUGUI[choices.Length];
+        int index = 0;
+        foreach(GameObject choice in  choices)
+        {
+            choicesText[index] = choice.GetComponentInChildren<TextMeshProUGUI>();
+            index++;
+        }
+
+        EnterDialogueMode(inkJSON);
+    }
+
+    private void Update()
+    {
+        if (!dialogueIsPlaying)
+        {
+            return;
+        }
+        if (Input.GetKeyDown("space"))
+        {
+            ContinueStory();
+        }
+    }
+
+    public void EnterDialogueMode(TextAsset inkJSON)
+    {
+        currentStory = new Story(inkJSON.text);
+        dialogueIsPlaying = true;
+        dialoguePanel.SetActive(true);
+        ContinueStory();
+    }
+
+    private void ExitDialogueMode()
+    {
+        Application.LoadLevel(LoadLevel);
+    }
+
+    private void ContinueStory()
+    {
+        if (currentStory.canContinue)
+        {
+            dialogueText.text = currentStory.Continue();
+            DisplayChoices();
+            HandleTags(currentStory.currentTags);
+        }
+        else
+        {
+            ExitDialogueMode();
+        }
+    }
+
+    private void HandleTags(List<string> currentTags)
+    {
+        foreach (string tag in currentTags)
+        {
+            string[] splitTag = tag.Split(":");
+
+            string tagKey = splitTag[0].Trim();
+            string tagValue = splitTag[1].Trim();
+
+            switch (tagKey)
+            {
+                case SPEAKER_TAG:
+                    displayNameText.text = tagValue;
+                    break;
+                case PORTRAIT_TAG:
+                    portraitAnimator.Play(tagValue);
+                    break;
+                default:
+                    break;
+            }
+        }
+        
+    }
+
+    private void DisplayChoices()
+    {
+        List<Choice> currentChoices = currentStory.currentChoices;
+        int index = 0;
+        foreach (Choice choice in currentChoices)
+        {
+            choices[index].gameObject.SetActive(true);
+            choicesText[index].text = choice.text;
+            index++;
+        }
+        for (int i = index; i < 2; i++)
+        {
+            choices[i].gameObject.SetActive(false);
+        }
+    }
+
+    public void MakeChoice(int choiceIndex)
+    {
+        currentStory.ChooseChoiceIndex(choiceIndex);
+        ContinueStory();
+    }
+}
