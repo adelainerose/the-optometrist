aa418854fd74812ba4743add11bc0d0392628bab
Nicole-Arii <nicole.lent@outlook.com>
2025-07-17 14:04:49 -0700

    LIGHTING!!!

---
+++ Assets/activate_flashlight.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class activate_flashlight : MonoBehaviour
+{
+    [SerializeField] private GameObject flashlight;
+    public static bool flashlightOn;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        flashlight.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("x"))
+        {
+            if (flashlightOn)
+            {
+                flashlight.SetActive(false);
+                flashlightOn = false;
+            }
+            else if (!flashlightOn)
+            {
+                flashlight.SetActive(true);
+                flashlightOn = true;
+            }
+        }
+    }
+}
